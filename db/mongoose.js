c624f7c501f68ca010ec6f8f94fb8d10954a89ea
daggim99 <programmerdaggim99@gmail.com>
2022-07-19 20:38:34 +0300

    VIP added and user add account fixed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017", {
+mongoose.connect("mongodb+srv://olyad-cinema:olyadcinemapassword@cluster0.qo5bg2z.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
-//"mongodb+srv://olyad-cinema:olyadcinemapassword@cluster0.qo5bg2z.mongodb.net/?retryWrites=true&w=majority"
+//
